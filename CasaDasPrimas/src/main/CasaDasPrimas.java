eaa3552643283e6b0ffc520ea2229a35f9cd964a
AlunEmanuel <badaro.fonseca@mail.uft.edu.br>
2025-02-17 15:12:10 -0300

    Estrutura inicial do projeto CasaDasPrimas

--- CasaDasPrimas/src/main/CasaDasPrimas.java
+++ CasaDasPrimas/src/main/CasaDasPrimas.java
@@ -2,6 +2,6 @@ package main;
 
 public class CasaDasPrimas {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World beach!");
     }
 }
